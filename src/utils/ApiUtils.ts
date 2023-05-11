506ef5bac56993f4126075e104dd89d8ca158f88
meidya456 <sjkang0228@gmail.com>
2023-05-11 14:35:40 +0900

    환경변수 추가, 디펜던시 정리, msw api mocking 추가

--- src/utils/ApiUtils.ts
+++ src/utils/ApiUtils.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
-
+import urls from "./Urls";
 const ApiUtils = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
+  baseURL: urls.movie.base,
   timeout: 10000,
   method: "get",
   params: {
-    api_key: "048aa1eeba36d0b1a3c7d78f14379ccf", //환경변수로 값을 셋팅하도록 한다.
+    api_key: process.env.NEXT_PUBLIC_API_KEY,
     language: "ko-KR",
     region: "KR",
   },
--- src/utils/Urls.ts
+++ src/utils/Urls.ts
@@ -1,1 +1,7 @@
-export default {};
+const movie = {
+  base: `https://api.themoviedb.org/3/`,
+  now: `/movie/now_playing`,
+  detail: (id: number) => `/movie/${id}`,
+};
+
+export default {movie};
