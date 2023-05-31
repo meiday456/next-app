f36e1326bd1481ec21aa557f34eeb83c6710379b
meidya456 <sjkang0228@gmail.com>
2023-05-31 21:35:04 +0900

    style(swiper): swiper a11y 추가, swiper item style
    swiper slide a11y 추가
    swiper item의 크기가 들쭉날쭉한 스타일 수정(반응형으로 일정비율을 유지)

--- src/components/molecules/item/Item.tsx
+++ src/components/molecules/item/Item.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 import Image from "next/image";
-import {CommonResult, ImageType} from "@/types/data/CommonType";
+import {CommonResult} from "@/types/data/CommonType";
 import urls from "@/utils/Urls";
 import {ImageLoaderProps} from "next/dist/shared/lib/image-config";
 import Link from "next/link";
@@ -8,12 +8,22 @@ import Link from "next/link";
 const ItemStyle = styled.div`
   position: relative;
   width: 100%;
-  height: 25vh;
+  padding: 70% 0;
   border-radius: 7px;
   overflow: hidden;
   object-fit: contain;
 
-  & .image {
+  a {
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 100%;
+    transform: translateY(-50%);
+    z-index: 5;
+  }
+
+  .image {
+    display: block;
     inset: 0;
     object-fit: cover;
   }
@@ -25,29 +35,28 @@ interface Props extends CommonResult {
 }
 const Item = (props: Props): React.ReactElement => {
   const imageLoader = ({src, width, quality}: ImageLoaderProps) => {
-    return `${urls.common.image(src, "content", width)}?w=${width}&q=${quality || 75}`;
+    return `${urls.common.image(src, "content", width)}?w=${width}&q=${quality ?? 75}`;
   };
 
   return (
     <ItemStyle>
-      <Link href={"#"}>
-        <Image
-          src={props.poster_path}
-          alt={`${props.title || props.name || "대체이미지"}`}
-          fill={true}
-          loader={imageLoader}
-          placeholder={"blur"}
-          loading={"lazy"}
-          blurDataURL={
-            "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOsaWm5AQAFZgJe6m0qkAAAAABJRU5ErkJggg=="
-          }
-          sizes="(max-width: 600) 200px,
+      <Link href={"#"}></Link>
+      <Image
+        src={props.poster_path}
+        alt={`${props.title ?? props.name ?? "대체이미지"}`}
+        fill={true}
+        loader={imageLoader}
+        placeholder={"blur"}
+        loading={"lazy"}
+        blurDataURL={
+          "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOsaWm5AQAFZgJe6m0qkAAAAABJRU5ErkJggg=="
+        }
+        sizes="(max-width: 600) 200px,
                 (max-width: 1000px) 300px,
                 (max-width: 1400px) 400px,
                 (max-width: 2000px) 500px, 100vw"
-          className={"image"}
-        ></Image>
-      </Link>
+        className={"image"}
+      ></Image>
     </ItemStyle>
   );
 };
